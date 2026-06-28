88073d1cf2ca422ec04aaeed3e77bb03f49747e4
gustavjung01 <gustavjung01@gmail.com>
2026-06-28 19:12:44 +0700

    Add public app config defaults

---
+++ app-config.js
@@ -0,0 +1,6 @@
+window.BEPI_CONFIG = Object.assign({}, window.BEPI_CONFIG || {}, {
+  supabaseUrl: 'https://noiadkpkvdohljgopgfb.supabase.co',
+  supabaseAnonKey: 'sb_publishable_n6LXv-fd-ImF3XzeU2mrjg_G7tBGy66',
+  agentName: 'Bếp Sỉ Report Analyst',
+  agentJson: '{"agents":[{"id":"bep-si-report-analyst","name":"Bếp Sỉ Report Analyst"}]}'
+});
