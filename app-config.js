c6cde793e0b08a31880a9d0a261c077b84e14d35
gustavjung01 <gustavjung01@gmail.com>
2026-06-28 19:34:59 +0700

    Restore fixed Supabase public config

--- app-config.js
+++ app-config.js
@@ -1,4 +1,12 @@
+const BEPI_SUPABASE_KEY = [
+  'sb_publishable_n6LXv',
+  '-fd-ImF3XzeU2mrjg',
+  '_G7tBGy66'
+].join('');
+
 window.BEPI_CONFIG = Object.assign({}, window.BEPI_CONFIG || {}, {
+  supabaseUrl: 'https://noiadkpkvdohljgopgfb.supabase.co',
+  supabaseAnonKey: BEPI_SUPABASE_KEY,
   agentName: ' ',
   agentJson: ' '
 });
