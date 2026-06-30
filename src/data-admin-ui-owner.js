cc5b5f598132e54a4ec783a1b0d7a1a406ede871
gustavjung01 <gustavjung01@gmail.com>
2026-06-30 22:31:58 +0700

    Load admin data tools

--- src/data-admin-ui-owner.js
+++ src/data-admin-ui-owner.js
@@ -10,3 +10,4 @@ import './order-manage-actions.js?v=order-detail-1';
 import './order-detail-enhance.js?v=order-detail-1';
 import './order-data-hub-enhance.js?v=order-data-hub-1';
 import './test-report-manage-actions.js?v=test-delete-fix-1';
+import './admin-data-tools.js?v=admin-data-tools-1';
