e7efd34e3294afc0b9ad8c0a3a2ecc870ef51992
gustavjung01 <gustavjung01@gmail.com>
2026-07-01 11:04:39 +0700

    Load price quote export tool

--- src/data-admin-ui-owner.js
+++ src/data-admin-ui-owner.js
@@ -6,3 +6,4 @@ import './order-data-overlap-hardfix.js?v=order-overlap-hardfix-1';
 import './order-data-layout-final.js?v=order-layout-final-1';
 import './test-report-manage-actions.js?v=test-delete-fix-1';
 import './admin-data-tools.js?v=admin-data-tools-1';
+import './price-quote-export.js?v=price-quote-1';
